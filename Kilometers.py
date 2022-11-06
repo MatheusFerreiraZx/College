517457e24281dd7e8cbd3decf3a6f5bacd7e94c5
Matheus Ferreira da Silva Nascimento <Mathferreiranasc12@gmail.com>
2022-11-06 20:28:35 -0300

    Create Kilometers.py

---
+++ Kilometers.py
@@ -0,0 +1,6 @@
+km = int(input('quantos km foram percorridos?'))
+dias = int(input('Por quantos dias o carro foi alugado?'))
+
+preco = 60 * dias + 0.15 * km
+print('Km = {}, Dias: {}'.format(km, dias))
+print('valor a ser pago: {}'.format(preco)
