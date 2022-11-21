9cccea500293dce8c052bb3d46d1e2ee976045e0
MatheusFerreiraZx <mathferreiranasc12@gmail.com>
2022-11-21 00:47:47 -0300

    feature/ new apps and refactoring old codes

---
+++ Lógica de programação e Algoritmos/Serviço de Limpeza/ServiçodeLimpeza.py
@@ -0,0 +1,74 @@
+#Apresentação
+print("Seja bem vindo ao App de Serviço de Limpeza do Matheus Ferreira da Silva Nascimento,\n "
+"Registro Uninter: 4282406")
+
+#Função metragem da limpeza
+def metragem_limpeza():
+    metragem = input("Qual a metragem da porção a ser limpa? ")
+
+    try:
+
+        metragem = int(metragem)
+        if not 30 <= metragem < 700:
+            metragem_limpeza()
+
+        else:
+            metragem = int(metragem)
+            if 30 <= metragem < 300:
+                valor_metragem = 60 + 0.3 * metragem
+            else:
+                valor_metragem = 120 + 0.5 * metragem
+            return valor_metragem
+
+    except:
+
+        metragem_limpeza()
+
+#Função tipo de limpeza
+def tipo_limpeza():
+    tipo = input("Qual o tipo de limpeza será feita? Digite B para básica e C para completa. ")
+    if tipo == "B":
+        multiplicador = 1.00
+    elif tipo == "C":
+        multiplicador = 1.30
+    else:
+        tipo_limpeza()
+    return multiplicador
+
+
+#Função adicional da limpeza
+def adicional_limpeza():
+    aux = 0
+    print(Tipos_adicionais)
+
+    while True:
+
+        adicional = input("Digite o número do adicional que deseja. ")
+        if int(adicional) == 0:
+            break
+        elif int(adicional) in Tipos_adicionais:
+            aux = aux + Valor_adicional[int(adicional)]
+        else:
+            continue
+    return aux
+
+
+# tabela adicionais
+
+Tipos_adicionais = {0: "Sem adicionais",
+
+                   1: "Passar 10 peças de roupa",
+
+                   2: "Limpeza de Forno/Microondas",
+
+                   3: "Limpeza de Freezer/Geladeira"}
+
+Valor_adicional = [0, 10.00, 12.00, 20.00]
+
+#Programa
+valor = metragem_limpeza()
+tipo_limpeza = tipo_limpeza()
+adicionais = adicional_limpeza()
+
+total = (valor * tipo_limpeza) + adicionais
+print(f"R$ {total:,.2f}")
---
+++ Lógica de programação e Algoritmos/Sorveteria/Sorveteria.py
@@ -0,0 +1,46 @@
+#Apresentação
+print("Seja bem vindo a Sorveteria do Matheus Ferreira da Silva Nascimento,\n "
+"Registro Uninter: 4282406")
+
+print("----------------------------------Carpádio---------------------------------------------------")
+print("Código | Descrição            | Tamanho P (500ml) | Tamanho M (1000ml) | Tamanho G (2000ml) |")
+print(" TR    | Sabores Tradicionais |      R$ 6,00      |      R$ 10,00      |     R$ 18,00       |")
+print(" ES    | Sabores Especiais    |      R$ 7,00      |      R$ 12,00      |     R$ 21,00       |")
+print(" PR    | Sabores Premium      |      R$ 8,00      |      R$ 14,00      |     R$ 24,00       |")
+print("---------------------------------------------------------------------------------------------")
+
+#Variáveis
+Tamanho = ["P", "M", "G"]
+
+Codigos = {"TR": [6.00, 10.00, 18.00],
+
+           "ES": [7.00, 12.00, 21.00],
+
+           "PR": [8.00, 14.00, 21.00]}
+
+Compra = []
+
+#Condição
+while True:
+
+    qual_tamanho = input("Qual o tamanho do sorvete desejado? ")
+
+    qual_sabor = input("Qual o código do sorvete desejado? ")
+
+    if qual_tamanho in Tamanho and qual_sabor in Codigos:
+
+        pedido = Codigos[qual_sabor][Tamanho.index(qual_tamanho)]
+        Compra.append(pedido)
+        algo_mais = input("Deseja pedir algo mais?"
+
+                          "\nDigite S para sim ou N para não. ")
+
+        if algo_mais == "S":
+            continue
+        else:
+            break
+    else:
+        print("TAMANHO ou CÓDIGO inválido(s)")
+        continue
+
+print("Valor total da compra:", "R$", sum(Compra))
